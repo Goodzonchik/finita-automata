e155f372fdca5e67e51ef3696a914c9d39d394ba
Sergey Glazyrin <goodzonchik@rambler.ru>
2024-07-05 09:01:01 +0500

    update finita automata

--- finita-automata.js
+++ finita-automata.js
@@ -1,25 +1,85 @@
-var FinitaAutomata = /** @class */ (function () {
-    function FinitaAutomata(states, initialStatus) {
+class FinitaAutomata {
+    states;
+    currentStatus;
+    moveHistory = [];
+    constructor(states, initialStatus) {
         this.states = states;
-        var initStatus = this.states.find(function (state) { return state.status === initialStatus; });
+        const initStatus = this.states.find(state => state.status === initialStatus);
         if (initStatus) {
-            this.currentStatus = initStatus;
+            this.currentStatus = initStatus.status;
+            this.saveHistory({
+                statusTo: initStatus.status
+            });
         }
         else {
             throw 'initial status not found in statuses';
         }
     }
-    FinitaAutomata.prototype.getStates = function () {
-        console.log(this.states);
-    };
-    return FinitaAutomata;
-}());
-var x = new FinitaAutomata([{
+    getStates() {
+        return this.states;
+    }
+    getAvilableMoves() {
+        return this.states.find(state => state.status === this.currentStatus)?.avalableState;
+    }
+    isStatus(status) {
+        return this.currentStatus === status;
+    }
+    canMoveToStatus(status) {
+        return this.getAvilableMoves()?.includes(status);
+    }
+    moveToStatus(status, callback) {
+        if (this.canMoveToStatus(status)) {
+            try {
+                if (callback) {
+                    callback();
+                }
+                this.saveHistory({
+                    callback,
+                    statusFrom: this.currentStatus,
+                    statusTo: status
+                });
+                this.currentStatus = status;
+            }
+            catch {
+                throw 'can`t call callback';
+            }
+        }
+        else {
+            throw `can't move to status ${status}`;
+        }
+    }
+    getHistory() {
+        return this.moveHistory.map(item => {
+            return `${item.statusFrom ? `${item.statusFrom} -> ` : ''}${item.statusTo}`;
+        });
+    }
+    saveHistory({ statusFrom, statusTo, callback }) {
+        this.moveHistory.push({
+            statusFrom,
+            statusTo,
+            callback
+        });
+    }
+}
+const automata = new FinitaAutomata([{
         status: 'draft',
         avalableState: ['published', 'deleted']
     },
     {
         status: 'published',
+        avalableState: ['draft', 'archived']
+    },
+    {
+        status: 'archived',
         avalableState: ['draft', 'deleted']
+    },
+    {
+        status: 'deleted',
+        avalableState: []
     }], 'draft');
-console.log(x.getStates());
+console.log(automata.getStates());
+console.log(automata.getAvilableMoves());
+automata.moveToStatus('published');
+automata.moveToStatus('archived');
+automata.moveToStatus('deleted');
+console.log(automata.getHistory());
--- finita-automata.ts
+++ finita-automata.ts
@@ -1,4 +1,4 @@
-type StateStatus = 'draft' | 'published' | 'deleted';
+type StateStatus = 'draft' | 'published' | 'archived' |'deleted';
 
 type State<T> = {
     status: T,
@@ -7,17 +7,25 @@ type State<T> = {
 
 class FinitaAutomata<T> {
     private states: State<T>[]
+    private currentStatus: T;
 
-    private currentStatus: State<T>;
+    private moveHistory: {
+        statusFrom?: T ,
+        statusTo: T,
+        callback?: () => void 
+    }[] = [];
 
     constructor(states: State<T>[], initialStatus: T) {
         this.states = states;
 
-
         const initStatus = this.states.find(state => state.status === initialStatus);
 
         if (initStatus){
-            this.currentStatus = initStatus;
+            this.currentStatus = initStatus.status;
+            
+            this.saveHistory({
+                statusTo:initStatus.status
+            })
         } else {
             throw 'initial status not found in statuses';
         }
@@ -25,17 +33,81 @@ class FinitaAutomata<T> {
     }
 
     getStates(){
-        console.log(this.states);
+        return this.states;
+    }
+
+    getAvilableMoves(){
+       return this.states.find(state => state.status === this.currentStatus)?.avalableState
+    }
+
+    isStatus(status: T){
+        return this.currentStatus === status;
+    }
+
+    canMoveToStatus(status: T){
+        return this.getAvilableMoves()?.includes(status);
+    }
+
+    moveToStatus(status: T, callback?: () => void){
+        if(this.canMoveToStatus(status)){
+            try{
+                if (callback){
+                    callback();
+                }
+                
+                this.saveHistory({
+                    callback,
+                    statusFrom: this.currentStatus,
+                    statusTo: status    
+                })
+                this.currentStatus = status;
+            }catch{
+                throw 'can`t call callback';
+            }
+        } else {
+            throw `can't move to status ${status}`;
+        }
+    }
+
+    getHistory(){
+        return this.moveHistory.map(item => {
+            return `${item.statusFrom ? `${item.statusFrom} -> ` : ''}${item.statusTo}`
+        });
+    }
+
+    private saveHistory({statusFrom, statusTo, callback}: {statusFrom?: T, statusTo: T, callback?: () => void}) {
+        this.moveHistory.push({
+            statusFrom,
+            statusTo,
+            callback})
     }
 }
 
-const x = new FinitaAutomata<StateStatus>([{
+const automata = new FinitaAutomata<StateStatus>([{
     status: 'draft',
     avalableState: ['published', 'deleted']
 },
 {
     status: 'published',
+    avalableState: ['draft', 'archived']
+},
+{
+    status: 'archived',
     avalableState: ['draft', 'deleted']
+},
+{
+    status: 'deleted',
+    avalableState: []
 }], 'draft');
 
-console.log(x.getStates())
+console.log(automata.getStates())
+console.log(automata.getAvilableMoves())
+
+automata.moveToStatus('published');
+
+automata.moveToStatus('archived');
+
+automata.moveToStatus('deleted');
+
+
+console.log(automata.getHistory())
